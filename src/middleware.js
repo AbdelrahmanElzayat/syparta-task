6626a7f33310167af5918f3ed38e04cf4de17ae6
AbdelrahmanElzayat <bodyelmajec@gmail.com>
2024-09-03 09:25:47 +0300

    second commit

--- src/middleware.js
+++ src/middleware.js
@@ -3,12 +3,23 @@ import { NextResponse } from "next/server";
 export function middleware(request) {
   const token = request.cookies.get("token")?.value;
   const url = request.url;
+
+  // إذا لم يكن هناك توكن و المستخدم يحاول الوصول إلى صفحة تسجيل الدخول
+  if (!token && url.includes("/login")) {
+    return NextResponse.next(); // لا تقم بإعادة التوجيه
+  }
+
+  // إذا لم يكن هناك توكن و المستخدم يحاول الوصول إلى أي صفحة محمية
   if (!token) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
+
+  // إذا كان هناك توكن و المستخدم يحاول الوصول إلى صفحة تسجيل الدخول
+  // يمكن إعادة التوجيه إلى صفحة أخرى (مثل لوحة التحكم)
   if (token && url.includes("/login")) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
+
   return NextResponse.next();
 }
 
